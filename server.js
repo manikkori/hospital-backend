a316656339a1eaf3fc75ed24b919acaddffefecb
Manik kori <manikkori697@gmail.com>
2026-05-31 10:55:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const Appointment = mongoose.model('Appointment', appointmentSchema);
 // Contact Inquiry Schema
 const inquirySchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true },
+   phone: { type: String, required: true },
     subject: { type: String, required: true },
     message: { type: String, required: true },
     status: { type: String, default: 'Unread' }
